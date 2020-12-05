6891fbfa4e91415072a2a73b704c61c0197ed66d
swalsh22 <steven.james.walsh@gmail.com>
2020-12-05 22:21:19 +0000

    cant get functions to work

--- hw6.rb
+++ hw6.rb
@@ -1,5 +1,5 @@
 
-def evaluate(ex, envi)
+def evaluate(ex, envi, funk)
   
   case ex
     when IntConstant    
@@ -7,8 +7,8 @@ def evaluate(ex, envi)
     when BooleanConstant    
       return BooleanValue.new(ex.c)
     when BinaryOperationExpression
-      leftValue = IntValue.new(evaluate(ex.left,envi)).v
-      rightValue = IntValue.new(evaluate(ex.right,envi)).v
+      leftValue = IntValue.new(evaluate(ex.left,envi,funk)).v
+      rightValue = IntValue.new(evaluate(ex.right,envi,funk)).v
       
       case ex.op
         when BinaryOperationExpression::Operator::PLUS
@@ -23,8 +23,8 @@ def evaluate(ex, envi)
           raise "Unkown Binary Operation"
       end
     when ComparisonExpression
-      left = evaluate(ex.left,envi).v
-      right = evaluate(ex.right,envi).v
+      left = evaluate(ex.left,envi,funk).v
+      right = evaluate(ex.right,envi,funk).v
 
       case ex.type
         when ComparisonExpression::TYPE::EQ
@@ -37,21 +37,38 @@ def evaluate(ex, envi)
     
     
     when IfExpression
-      cond = evaluate(ex.condition,envi)
+      cond = evaluate(ex.condition,envi,funk)
       if(cond.b)
-        return evaluate(ex.thenSide,envi)
+        return evaluate(ex.thenSide,envi,funk)
       else
-        return evaluate(ex.elseSide,envi)
+        return evaluate(ex.elseSide,envi,funk)
       end
     when LetExpression
-      v = evaluate(ex.value, envi)
+      v = evaluate(ex.value, envi,funk)
       envi.bind(ex.variable, v)
-      return evaluate(ex.body, envi)
+      return evaluate(ex.body, envi,funk)
     when VariableExpression
       return envi.lookup(ex.variable)
+    when FunctionCallExpression
+      foundfunk = findfunction(ex.functionName,funk)
+      
+      evalEnvi = envi
 
+      foundfunk.formalNamesOfArguments.length().times {|i|
+        actualArgumentsVal = evaluate(ex.actualArguments[i],ex,funk)
+        evalEnvi = evalEnvi.bind(foundfunk.name, actualArgumentsVal)     
+      }  
+      return evaluate(foundfunk.body, evalEnvi, funk)
+  end    
+end
 
-    end    
+def findfunction(name, funk)
+  funk.each do |item|
+    if(item.name.equals(name.variable))
+      return item
+    end
+  end
+  raise 'cant find function'
 end
 
 
@@ -164,6 +181,19 @@ class VariableExpression < Expression
     @variable = variable
   end
 end
+class FunctionCallExpression < Expression
+  def functionName
+    @functionName
+  end
+  def actualArguments
+    @actualArguments
+  end
+
+  def initialize(functionName, *actualArguments)
+    @functionName = functionName
+    @actualArguments = actualArguments
+  end
+end
 
 # values
 class Value
@@ -195,8 +225,6 @@ class BooleanValue < Value
   end
 end
 
-
-
 class Bindings
   def name
     @name
@@ -221,6 +249,8 @@ class Environmnet
     BINDS.each do |item|
       if (item.name.equals(name))
         return item.value
+      else 
+        raise 'lookup cant find'
       end
     end
   end
@@ -235,7 +265,6 @@ class DynamicScopedEnvironment < Environmnet
     BINDS.unshift(Bindings.new(name, value))
   end
 end
-
 
 class Name
   def theName
@@ -260,13 +289,31 @@ class Name
 
 end
 
+class Function
+  def name
+    @name
+  end
+  def body
+    @body
+  end
+  def formalNamesOfArguments
+    @formalNamesOfArguments
+  end
+
+  def initialize (name, body, *formalNamesOfArguments)
+    @name = name
+    @body = body
+    @formalNamesOfArguments = formalNamesOfArguments
+  end
+end
 
 envi = DynamicScopedEnvironment.new
+funk = []
 # Problems
 # 474
 p1 = IntConstant.new(474)
 puts 'p1'
-puts evaluate(p1,envi)
+puts evaluate(p1,envi,funk)
 
 # (400 + 74) / 3
 p2 = BinaryOperationExpression.new(
@@ -278,7 +325,7 @@ p2 = BinaryOperationExpression.new(
   IntConstant.new(3)
 )
 puts 'p2'
-puts evaluate(p2,envi)
+puts evaluate(p2,envi,funk)
 
 #((400 + 74) / 3) == 158
 p3 = ComparisonExpression.new(
@@ -294,7 +341,7 @@ p3 = ComparisonExpression.new(
   IntConstant.new(158)
 )
 puts 'p3'
-puts evaluate(p3,envi)
+puts evaluate(p3,envi,funk)
 
 # if (((400 + 74) / 3) == 158) then 474 else 474/0
 p4 = IfExpression.new(
@@ -307,7 +354,7 @@ p4 = IfExpression.new(
   )
 )
 puts 'p4'
-puts evaluate(p4,envi)
+puts evaluate(p4,envi,funk)
 
 #let bot = 3 in
 #   (let bot = 2 in bot)
@@ -349,7 +396,7 @@ p5 = LetExpression.new(
 )
 
 puts 'p5'
-puts evaluate(p5,envi)
+puts evaluate(p5,envi,funk)
 
 
 
@@ -375,5 +422,105 @@ p5b = LetExpression.new(
   )
 )
 puts 'p5b'
-puts evaluate(p5b,envi)
+puts evaluate(p5b,envi,funk)
 
+
+
+f = Function.new(
+  Name.new('f'),
+  IfExpression.new(
+    ComparisonExpression.new(
+      ComparisonExpression::TYPE::EQ,
+      VariableExpression.new('bot'),
+      IntConstant.new(0)
+    ),
+    IntConstant.new(0),
+    BinaryOperationExpression.new(
+      BinaryOperationExpression::Operator::DIV,
+      VariableExpression.new('top'),
+      VariableExpression.new('bot')
+    )
+  ),
+  VariableExpression.new('top'),
+  VariableExpression.new('bot')
+)
+
+funk.push(f)
+
+
+safeDivision = Function.new(
+  Name.new('safeDivision'),
+  IfExpression.new(
+    ComparisonExpression.new(
+      ComparisonExpression::TYPE::EQ,
+      VariableExpression.new(Name.new('bot')),
+      IntConstant.new(0)
+    ),
+    IntConstant.new(0),
+    BinaryOperationExpression.new(
+      BinaryOperationExpression::Operator::DIV,
+      VariableExpression.new(Name.new('top')),
+      VariableExpression.new(Name.new('bot'))
+    )
+  ),
+  Name.new('top'),
+  Name.new('bot')
+)
+
+funk.push(safeDivision)
+
+p8 = FunctionCallExpression.new(
+  VariableExpression.new(Name.new('safeDivision')),
+  IntConstant.new(474),
+  BinaryOperationExpression.new(
+    BinaryOperationExpression::Operator::MINUS,
+    IntConstant.new(474),
+    BinaryOperationExpression.new(
+      BinaryOperationExpression:: Operator::PLUS,
+      IntConstant.new(400),
+      IntConstant.new(74)
+    )
+  )
+)
+
+
+#puts 'p8'
+#puts evaluate(p8,envi,funk)
+
+
+p6 = LetExpression.new(
+  Name.new('bot'),
+  IntConstant.new(3),
+  BinaryOperationExpression.new(
+    BinaryOperationExpression::Operator::PLUS,
+    LetExpression.new(Name.new('bot'),
+      IntConstant.new(2),
+      VariableExpression.new(Name.new('bot'))
+    ),
+    BinaryOperationExpression.new(
+      BinaryOperationExpression::Operator::PLUS,
+      FunctionCallExpression.new(
+        VariableExpression.new(Name.new('f')),
+        BinaryOperationExpression.new(
+          BinaryOperationExpression::Operator::PLUS,
+          IntConstant.new(400),
+          IntConstant.new(74)
+        ),
+        VariableExpression.new(Name.new('bot'))
+      ),
+      FunctionCallExpression.new(
+        VariableExpression.new(Name.new('f')),
+        BinaryOperationExpression.new(
+          BinaryOperationExpression::Operator::PLUS,
+          IntConstant.new(470),
+          IntConstant.new(4)
+        ),
+        IntConstant.new(0)
+      )
+    )
+  
+  )
+)
+
+#puts 'p6'
+#puts evaluate(p6,envi,funk)
